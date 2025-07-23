16f30a89bb02798151230192a962367d05107b43
Celestin Fernandes <celestin.fernandes@email.igenesys.com>
2025-07-23 23:08:06 +0530

    Removing unnecessary file

--- job-portal/src/components/Nav.jsx
+++
@@ -1,8 +0,0 @@
-import { NavLink } from "react-router-dom";
-
-const Nav = () => {
-    return <>
-    <NavLink to="/about">About</NavLink>
-    <NavLink to="/projects">Projects</NavLink>
-    </>
-};
