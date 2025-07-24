8e7cb6a7e7b5cda1b53f0a0304cfbd3427a77830
Celestin Fernandes <celestin.fernandes@email.igenesys.com>
2025-07-24 15:47:53 +0530

    Img and other updates

--- job-portal/src/components/JobCat.jsx
+++
@@ -1,7 +0,0 @@
-export default function JobCategories({ isDark }) {
-  const categories = [
-    "Software Developer","Lead Data Scientist",
-    "System Administrator","Data Analyst","Frontend Developer",
-    "Backend Developer","Full Stack Developer","DevOps Engineer"]
-  return null
-}
